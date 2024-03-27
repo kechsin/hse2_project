4c3090f27b4934ac8d34834ad872dc1bfb1b112d
kechsin <evlepikhina_1@edu.hse.ru>
2024-03-27 17:18:13 +0300

    ids instead of urls in channels list; small change in cluster_get

--- channels_to_json.py
+++ channels_to_json.py
@@ -3,94 +3,94 @@ channels = {"music": [], "politics": [], "science": [], "art": [],
             "sport": [], "funny": [], "games": []}
 
 channels["music"] = [
-    "https://www.youtube.com/@aquarium1972",  # 1 Аквариум
-    "https://www.youtube.com/@mmdcrew",  # 2 Моргенштерн
-    "https://www.youtube.com/@queenhouse-7874",  # 3
-    "https://www.youtube.com/@theBESTMUSICA",  # 4
-    "https://www.youtube.com/@worldvideoview5742",  # 5
-    "https://www.youtube.com/@mosconsvtv",  # 6 Консерватория
-    "https://www.youtube.com/@VelvetMusicRu",  # 7
-    "https://www.youtube.com/@muztv",  # 8 МузТВ
-    "https://www.youtube.com/@karaokekalinka",  # 9
-    "https://www.youtube.com/@gtrfmusic" # 10 из советских фильмов
+    "UCawVzkkGUvxTvwn_c_WJ-lA",  # 1 Аквариум
+    "UCWnqnojAgMdN0fQpr_xByJw",  # 2 Моргенштерн
+    "UC18bHNKD21woNT7G0F4PN7Q",  # 3
+    "UCSGRPrxhTRVMvmvqP4cXSjQ",  # 4
+    "UCljnQxyQFCELOb7A9v1t66w",  # 5
+    "UCDmgUB1w5SJVdIgVqy-Kgwg",  # 6 Консерватория
+    "UCTvLTz1K83pEOZucc04Hbwg",  # 7
+    "UC2eH4vw5JuVpzLDrCkxW2pQ",  # 8 МузТВ
+    "UC2XLrPeYgf0n1W2HG4XgXpA",  # 9
+    "UC7QX724vt3YwVnkMwtmdqNA"  # 10 из советских фильмов
 ]
 
 channels["politics"] = [
-    "https://www.youtube.com/@Popularpolitics",  # 1
-    "https://www.youtube.com/@khodorkovskylive",  # 2
-    "https://www.youtube.com/@Max_Katz",  # 3
-    "https://www.youtube.com/@MackNack",  # 4
-    "https://www.youtube.com/@LdprRuparty",  # 5
-    "https://www.youtube.com/@NavalnyRu",  # 6
-    "https://www.youtube.com/@ibertarian-party-ru",  # 7
-    "https://www.youtube.com/@CommunistPartyRF",  # 8
-    "https://www.youtube.com/@Ekaterina_Schulmann",  # 9
-    "https://www.youtube.com/@SuperSharij"  # 10
+    "UC7Elc-kLydl-NAV4g204pDQ",  # 1
+    "UCBzDAjLfvBUBVMMP6-K-y0w",  # 2
+    "UCUGfDbfRIx51kJGGHIFo8Rw",  # 3
+    "UCgpSieplNxXxLXYAzJLLpng",  # 4
+    "UCUT0cgd_Ru_Zykp4nP9B42g",  # 5
+    "UCsAw3WynQJMm7tMy093y37A",  # 6
+    "UCOu7RZ6qGjdFEeMiZQVtMXA",  # 7
+    "UCbNYBIHgdRw5LX_NK5-9qDA",  # 8
+    "UCL1rJ0ROIw9V1qFeIN0ZTZQ",  # 9
+    "UCtO0TzSAoIOzTnTsQeywSSw"   # 10
 ]
 
 channels["art"] = [
-    "https://www.youtube.com/@Gleb_Oleinik",  # 1 музыка
-    "https://www.youtube.com/@anna_vilenskaya",  # 2 музыка
-    "https://www.youtube.com/@MusicwithMarie15",  # 3 музыка
-    "https://www.youtube.com/@armenifedor",  # 4 литература
-    "http://www.youtube.com/@samosoboymedia",  # 5
-    "https://www.youtube.com/@user-kl6vv2do1v",  # 6 изобразительное
-    "https://www.youtube.com/@TheArtsmuseum",  # 7 изобразительное
-    "https://www.youtube.com/@GARAGEMCA",  # 8 современное искусство
-    "https://www.youtube.com/@TheBadComedian",  # 9 кино
-    "https://www.youtube.com/@kinopoisk"  # 10 кино
+    "UC68e0hrTENfQkhSxpnw2GMQ",  # 1 музыка
+    "UC9KFvnvWDmKAQhfxxDI0DHQ",  # 2 музыка
+    "UCkWGIBcWtSuJ7xu72tLCV4A",  # 3 музыка
+    "UCGzg7kiBe7AiTJQYg4TN1jQ",  # 4 литература
+    "UCoFIica17mSL70pXNFRHCWQ",  # 5 литература
+    "UCFs57_AuDKT02x9YaOEQ8Ww",  # 6 изобразительное
+    "UCw-SvnADFRiC87aKAuzZcXQ",  # 7 изобразительное
+    "UCSZujkjLtAKbTCieGdpDP5Q",  # 8 современное искусство
+    "UC6cqazSR6CnVMClY0bJI0Lg",  # 9 кино
+    "UC4tlrTXCBw6NPZ9nCA3_s9w"  # 10 кино
 ]
 
 channels["science"] = [
-    "https://www.youtube.com/@R_Nauka",  # 1
-    "https://www.youtube.com/@childrenscience",  # 2
-    "https://www.youtube.com/@NaukaPRO",  # 3
-    "https://www.youtube.com/@getaclassmath",  # 4
-    "https://www.youtube.com/@SciencePub",  # 5
-    "https://www.youtube.com/@AcademiaNauk",  # 6
-    "https://www.youtube.com/@user-wn9il6nl5l",  # 7
-    "https://www.youtube.com/@postnauka",  # 8
-    "https://www.youtube.com/@GTVscience",  # 9
-    "https://www.youtube.com/@MadAstronomer"  # 10
+    "UCDK8SFd2iuF_h4kklUoueJw",  # 1
+    "UCf053FwQD-hnTY7aaaWTaVQ",  # 2
+    "UClk8C-ve3vb96jSqltT05wA",  # 3
+    "UChE2sc5N7PfdV-yN2_ctvtg",  # 4
+    "UCl4goMIACgEkjZZAAiaWwJw",  # 5
+    "UC5yvMRG0e_3jl9xo5rr0DnA",  # 6
+    "UC_ruhy05BA2V_oxWjgTMlmA",  # 7
+    "UCSPd93is2UQsd_jZ6yHBfqQ",  # 8
+    "UCzWnF-3UWAGNeK5fIkBmahg",  # 9
+    "UCdVX4yIGECc9q4eKjKVoJKQ"  # 10
 ]
 
 channels["sport"] = [
-    "https://www.youtube.com/@imagine_fitness",  # 1
-    "https://www.youtube.com/@Mote",  # 2
-    "https://www.youtube.com/@user-qy9zt7qv6o",  # 3
-    "https://www.youtube.com/@IvanKrasavin",  # 4
-    "https://www.youtube.com/@smartballet",  # 5
-    "https://www.youtube.com/@delaytelo",  # 6
-    "https://www.youtube.com/@MakeFitness",  # 7
-    "https://www.youtube.com/@yogactivelife",  # 8
-    "https://www.youtube.com/@YOGAwork",  # 9
-    "https://www.youtube.com/@badassel17"  # 10
+    "UCjYicQAYfN_MTzHbKqvL_dA",  # 1
+    "UCrZkmVymFwJDcttvHnsLxvA",  # 2
+    "UC-ANf8CYk6gfCUW8UzoJptw",  # 3
+    "UCNOo2oB5YJmaVsGzMv4VHxw",  # 4
+    "UCWpjKglD29XizsdYQCCEJRg",  # 5
+    "UCFFsQwmidm9iTyCwKgMh8bQ",  # 6
+    "UCmmvrWZAHC9OPivFncPjTfws",  # 7
+    "UCXA22L0X3bdXwHpufPuRv-w",  # 8
+    "UCdwRBTP5LBk86EtX5Iv_6ow",  # 9
+    "UCdEW3rqePdRGxvgnKkOmyxw"  # 10
 ]
 
 channels["funny"] = [
-    "https://www.youtube.com/@standupclubru",  # 1
-    "https://www.youtube.com/@vanya.usovich",  # 2
-    "https://www.youtube.com/@improcom",  # 3
-    "https://www.youtube.com/@fridaytv",  # 4
-    "https://www.youtube.com/@barinthecity",  # 5
-    "https://www.youtube.com/@KatyaAdushkinaa",  # 6
-    "https://www.youtube.com/@kvn",  # 7
-    "https://www.youtube.com/@SmetanaTV",  # 8
-    "https://www.youtube.com/@klikklak",  # 9
-    "https://www.youtube.com/@chickencurryshow"  # 10
+    "UC3cJiUuZlpF-pkzqvSskTpg",  # 1
+    "UC1uS4JFcc8KXZVoBSh3XVHA",  # 2
+    "UCpTuLPXfIOcsPbvb2SIwD8w",  # 3
+    "UCsk9ntn2afzIqInnx2jB8gw",  # 4
+    "UCUbJYQmp_gAQWtaZn0ddO1w",  # 5
+    "UCA0Xum6Bxx-rhmrog1A1ISQ",  # 6
+    "UCSZ69a-0I1RRdNssyttBFcA",  # 7
+    "UCqRwbeXjkNd2f5ExFJXAZxQ",  # 8
+    "UC5sSL56GJWCi4wsMpobTrAw",  # 9
+    "UCn9bv143ECsDMw-kJCNN7QA"  # 10
 ]
 
 channels["games"] = [
-    "https://www.youtube.com/@kuplinovplay",  # 1
-    "https://www.youtube.com/@Compot",  # 2
-    "https://www.youtube.com/@windy31LetsGoodPlays",  # 3
-    "https://www.youtube.com/@DTFru",  # 4
-    "https://www.youtube.com/user/IgromaniaVideo",  # 5
-    "https://www.youtube.com/user/StopGameRu",  # 6
-    "https://www.youtube.com/channel/UCOxQWb-OuyCSZadzWqrKTCQ",  # 7
-    "https://www.youtube.com/channel/UCYF2mxg48E5c3-jKpZoZSJA",  # 8
-    "https://www.youtube.com/channel/UCx02EI6ZIgxyg2MgdE_HM-g",  # 9
-    "https://www.youtube.com/user/YogusFirst"  # 10
+    "UCdKuE7a2QZeHPhDntXVZ91w",  # 1
+    "UC0cWWruNMb95iCBEOzPnjRA",  # 2
+    "UCZUrS0zDszsXI_5ir_tI3cg",  # 3
+    "UCBDLWj5X5D9bvBa3JIMMTIQ",  # 4
+    "UC_Q1vhf7wcR_zGlc5ahAg0A",  # 5
+    "UCq7JZ8ATgQWeu6sDM1czjhg",  # 6
+    "UCOxQWb-OuyCSZadzWqrKTCQ",  # 7
+    "UCYF2mxg48E5c3-jKpZoZSJA",  # 8
+    "UCx02EI6ZIgxyg2MgdE_HM-g",  # 9
+    "UCOmAtwe8UxKuj-qIdV77Iog"  # 10
 ]
 
 f = open("channels.json", "w")
