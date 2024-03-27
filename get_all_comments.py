277eb6d614bcec0643e465ff3c933d5780339653
kechsin <evlepikhina_1@edu.hse.ru>
2024-03-27 17:39:58 +0300

    minor fix

--- get_all_comments.py
+++ get_all_comments.py
@@ -1,5 +1,5 @@
 import cluster_get
-clusters = ["politics", "science", "art", "sport", "funny", "games"]
+clusters = ["music", "politics", "science", "art", "sport", "funny", "games"]
 for i in clusters:
     cluster_get.save_videos(i, 20)
     cluster_get.save_comments(i, 50)
