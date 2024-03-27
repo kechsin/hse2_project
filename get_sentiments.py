a113bd9a5625b5ceee656d67c243819662907340
kechsin <evlepikhina_1@edu.hse.ru>
2024-03-27 22:31:15 +0300

    made analysis

--- get_sentiments.py
+++ get_sentiments.py
@@ -1,6 +1,7 @@
 import torch
 from transformers import AutoModelForSequenceClassification
 from transformers import BertTokenizerFast
+import json
 
 tokenizer = BertTokenizerFast.from_pretrained('blanchefort/rubert-base-cased-sentiment-rusentiment')
 model = AutoModelForSequenceClassification.from_pretrained('blanchefort/rubert-base-cased-sentiment-rusentiment', return_dict=True)
@@ -15,6 +16,23 @@ def predict(text):
     return predicted
 
 
-line = "Просто безэмоциональный текст." * 300
-a = predict(line)
-print(a)
+clusters = ["music", "politics", "science", "art", "sport", "funny", "games"]
+
+clusters_sents = {}
+for i in clusters:
+    current_cluster = {'neut': 0, 'pos': 0, 'neg': 0}
+    f = open(f"data/comments_{i}.txt", encoding="utf8")
+    comments = f.readlines()
+    for com in comments:
+        a = predict(com.strip())
+        if a[0] == 0:
+            current_cluster['neut'] += 1
+        elif a[0] == 1:
+            current_cluster['pos'] += 1
+        elif a[0] == 2:
+            current_cluster['neg'] += 1
+    clusters_sents[i] = current_cluster
+
+f = open("sentiment_data.json", "w")
+f.write(json.dumps(clusters_sents))
+f.close()
